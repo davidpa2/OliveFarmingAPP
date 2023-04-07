4bf997d208c3adcbec2d3cb36a3a5e320c678094
David Padilla <davidpaag2002@gmail.com>
2023-04-07 13:32:18 +0200

    feat(app): Module organization

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./pages/auth-module/auth.module').then(m => m.AuthModule)
+  },
+  // {
+  //   path: '',
+  //   loadChildren: () => import('./pages/public-module/public.module').then(m => m.PublicModule)
+  // },
+  // {
+  //   path: 'private',
+  //   loadChildren: () => import('./pages/private-module/private.module').then(m => m.PrivateModule),
+  // }
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    // CommonModule
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
