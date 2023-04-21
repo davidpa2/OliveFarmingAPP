398bcc4e3c17352b55b2d6d97bbaa0d59df3dd57
David Padilla <davidpaag2002@gmail.com>
2023-04-21 13:24:41 +0200

    feat(capacitor): Generated android app

---
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -0,0 +1,15 @@
+package io.ionic.starter;
+
+import android.os.Bundle;
+
+import com.getcapacitor.BridgeActivity;
+
+import java.util.ArrayList;
+
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) { 
+    super.onCreate(savedInstanceState);
+    // this.init(savedInstanceState, new ArrayList<>())
+  }
+}
