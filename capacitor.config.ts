f9a317cf06561ce87406f953a2661bc54ac304e8
David Padilla <davidpaag2002@gmail.com>
2023-10-19 20:36:02 +0200

    feat(app): Added custom splash screen on Adroid 12 or higher

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,9 +2,27 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'OliveFarmingAPP',
+  appName: 'Olive Farming',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      launchFadeOutDuration: 3000,
+      backgroundColor: "#ffffffff",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+      // androidSpinnerStyle: "large",
+      // iosSpinnerStyle: "small",
+      // spinnerColor: "#999999",
+      splashFullScreen: true,
+      splashImmersive: true,
+      // layoutName: "launch_screen",
+      // useDialog: true,
+    },
+  },
 };
 
 export default config;
